c6ccf7e81577d6c35e130923a0b546e6394671ad
QP2000 <mkotoh@hotmail.com>
2014-12-21 01:02:47 +0900

    test1

---
+++ gittest/src/gittest/GitTestMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gittest;
+
+/**
+ * @author MyPC
+ *
+ */
+public class GitTestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
